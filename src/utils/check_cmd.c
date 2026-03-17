1fbae7ef2c14e3d05ef13410765455159be06bfb
Jdavid765 <david.canoduran@hotmail.com>
2026-03-17 14:52:58 +0100

    segfault

--- src/utils/check_cmd.c
+++ src/utils/check_cmd.c
@@ -6,21 +6,21 @@
 /*   By: canoduran <canoduran@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 23:18:14 by canoduran         #+#    #+#             */
-/*   Updated: 2026/03/16 18:53:23 by canoduran        ###   ########.fr       */
+/*   Updated: 2026/03/17 14:49:42 by canoduran        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	check_cmd(char *rl, t_env **ft_env)
+void	check_cmd(char *rl, t_all *all)
 {
 	if (ft_compare(rl, "export") == 0)
-		export_builtin(ft_env, rl);
+		export_builtin(all, rl);
 	if (ft_compare(rl, "pwd") == 0)
 		pwd_builtin();
 	if (ft_compare(rl, "exit") == 0)
 		exit_builtin((void *)rl);
 	if (ft_compare(rl, "env") == 0)
-		cmd_env(ft_env);
+		cmd_env(all);
 	return ;
 }
