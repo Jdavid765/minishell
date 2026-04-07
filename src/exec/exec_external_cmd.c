93fd06ca07ed3235fa3f6951793b377ef62608f4
Nicolas Fiora Del Fabro <nfiora-d@c2r9s10.42lausanne.ch>
2026-04-07 16:33:13 +0200

    changing path in t_parser

--- src/exec/exec_external_cmd.c
+++ src/exec/exec_external_cmd.c
@@ -26,6 +26,8 @@ void	child_single_external(t_all *all, t_parser *cmd)
 		exit(127);
 	}
 	new_env = re_build_env(all->env, NULL);
+	free(cmd->path);
+	cmd->path = before_path_check(all->env, cmd);
 	restore_original_signals(all);
 	execve(cmd->path, cmd->cmd_and_args, new_env);
 	perror("execve");
--- src/exec/exec_pipeline.c
+++ src/exec/exec_pipeline.c
@@ -116,6 +116,8 @@ void	execute_pipeline_cmd(t_all *all, t_parser *cmd)
 		exit(127);
 	}
 	envp = re_build_env(all->env, NULL);
+	free(cmd->path);
+	cmd->path = before_path_check(all->env, cmd);
 	restore_original_signals(all);
 	execve(cmd->path, cmd->cmd_and_args, envp);
 	perror("execve");
